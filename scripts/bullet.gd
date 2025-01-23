5a57dd5b73c7a12f619c951622c15a0e884b16cf
fankyu <fankydoesart@gmail.com>
2025-01-23 00:24:09 -0300

    amwopdmawf

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 @export var direction = 0
-@export var speed = 10
+@export var speed = 1200
 @export var lifetime = 1
 
 @onready var life = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	rotation = direction
-	position += Vector2(cos(direction) * speed,sin(direction) * speed)
+	position += Vector2(cos(direction) * speed * delta,sin(direction) * speed * delta)
 	life += delta
 	if life >= lifetime:
 		queue_free()
--- scripts/player_bear.gd
+++ scripts/player_bear.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const MAX_DASH = 3000.0
 
 @onready var dashVector = Vector2()
 
 @onready var sprite = $BearSprite
 @onready var animationPlayer = $BearSprite/AnimationPlayer
+@onready var dashBar = $DashBar
 
 @onready var cooldown = 0
 @onready var dashScene = preload('res://scenes/dash.tscn')
@@ -16,8 +18,11 @@ const JUMP_VELOCITY = -400.0
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&'Enter'):
+		dashBar.max_value = MAX_DASH
+		dashBar.show()
 		hold = true
 	if event.is_action_released(&'Enter'):
+		dashBar.hide()
 		hold = false
 		dashVector = Input.get_vector(&'Left',&'Right',&'Up',&'Down') * power
 		for i in 5:
@@ -30,22 +35,26 @@ func _input(event: InputEvent) -> void:
 			inst.global_position = global_position
 			await get_tree().create_timer(0.05).timeout
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	# Add the gravity.
 	var dir = Input.get_vector(&'Left',&'Right',&'Up',&'Down')
 	if hold:
-		power += 30
+		power = clamp(power + 3000 * delta,0,MAX_DASH)
+		dashBar.value = power
+		
 	else:
 		power = 0
 	if dir.x > 0:
 		sprite.flip_h = false
 	elif dir.x < 0:
 		sprite.flip_h = true
-	velocity = dir * SPEED + dashVector
 	if dir.length() == 0:
 		animationPlayer.play(&'idle')
 	else:
-		animationPlayer.stop()
-
-	dashVector = dashVector.lerp(Vector2(),0.1)
-	move_and_slide()
+		animationPlayer.play(&'walk')
+	if dashVector.length() > 200:
+		animationPlayer.play(&'run')
+	elif dashVector.length() > 50:
+		animationPlayer.play(&'slide')
+	dashVector = dashVector.lerp(Vector2(),10 * delta)
+	position += (dir * SPEED * delta )+ (dashVector * delta)
