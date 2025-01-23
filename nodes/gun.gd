76acd93e0a204a51a29cd4e6c32c229f3feb9ad4
fankyu <fankydoesart@gmail.com>
2025-01-23 00:30:28 -0300

    cam shake

--- nodes/gun.gd
+++ nodes/gun.gd
@@ -17,6 +17,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+	cooldown = max(0,cooldown - delta)
 	if global_rotation_degrees > 90 or global_rotation_degrees < -90:
 		sprite.flip_v = true
 		
@@ -28,4 +29,8 @@ func _process(delta: float) -> void:
 			$'../..'.add_child(inst)
 			inst.direction = self.global_rotation
 			inst.position = sprite.global_position
+			if $'../..'.find_child('Camera2D'):
+				var cam = $'../..'.find_child('Camera2D')
+				cam.position += Vector2(randf_range(-10,10),randf_range(-10,10))
+			cooldown = 0.1
 			
